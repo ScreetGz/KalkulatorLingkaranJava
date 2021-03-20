53fde704a38cb7ba1e21bab45cd72544291c0c90
Screet <screetgz@gmail.com>
2021-03-20 14:13:19 +0700

    Update programKalkulator.java

--- programKalkulator.java
+++ programKalkulator.java
@@ -14,29 +14,26 @@ import java.util.Scanner;
 public class programKalkulator {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        Double luasLingkaran, kelilingLingkaran, jarijari, phi = 3.14;
-        
+        Double jarijari, phi = 3.14;
         int inputMenu;
-        System.out.println("==MENU==\n1.Hitung Luas lingkaran\n2.Hitung Keliling lingkaran\nInput Menu : ");
+        
+        System.out.println("MENU\n1.Hitung Luas lingkaran\n2.Hitung Keliling lingkaran\nPilih Menu : ");
         inputMenu = scan.nextInt();
         
         switch(inputMenu){
             case 1 -> {
+                Double luas;
                 System.out.println("Input Jari-jari lingkaran : ");
                 jarijari = scan.nextDouble();
-            
-                luasLingkaran = phi*jarijari*jarijari;
-            
-                System.out.println("Luas lingkaran adalah "+luasLingkaran);
+                luas = phi*jarijari*jarijari;
+                System.out.println("Luas lingkaran = "+luas);
             }
-            
             case 2 -> {
+                Double keliling;
                 System.out.println("Input Jari-jari lingkaran : ");
                 jarijari = scan.nextDouble();
-                
-                kelilingLingkaran = 2*phi*jarijari;
-                
-                System.out.println("Keliling lingkaran adalah : "+kelilingLingkaran);
+                keliling = 2*phi*jarijari;
+                System.out.println("Keliling lingkaran = "+keliling);
             }
                 
             default -> System.out.println("Inputan Anda Salah!");
